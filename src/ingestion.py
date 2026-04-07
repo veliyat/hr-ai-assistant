b1cae1d2cab7e8ee0d8a02023e6456e26ce3cb5a
Ravi Veliyat <veliyat@gmail.com>
2026-04-07 21:43:57 +0530

    feat: implement PDF ingestion and text splitting logic

--- src/ingestion.py
+++ src/ingestion.py
@@ -7,7 +7,14 @@ def process_pdf(file_path: str):
     pages = loader.load()
 
     #Step 2: Initialze the Splitter
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
 
     #Step 3: Split the documents into chunks
+    chunks = text_splitter.split_documents(pages)
 
-    #return chunks
+    return chunks
